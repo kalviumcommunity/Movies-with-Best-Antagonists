d6605c2569f8ab7a780a2749d1d1216662819dc3
shaaaaz <jiwanishaaz@gmail.com>
2024-02-24 11:50:12 +0530

    MongoDB Connected

---
+++ db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require("dotenv").config()
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB Connected!")
+    }
+    catch(err){
+        console.log("Database Connection Unseccusful")
+        process.exit(1)
+    }
+}
+
+const disconnectDB = async () => {
+    await mongoose.disconnect()
+    console.log("MongoDB disconnected")
+    process.exit(0)
+}
+
+const isConnected = () => {
+    return mongoose.connection.readyState === 1;
+}
+
+module.exports = { connectDB , disconnectDB , isConnected}
